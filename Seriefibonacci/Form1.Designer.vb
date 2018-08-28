49e863b4c0a9963e34eec5a61b745231230905e0
zeris <brandon-gg@outlook.com>
2018-08-27 22:59:46 -0500

    Initial Commit

---
+++ Seriefibonacci/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Serie_fibonacci
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cs = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.salida = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.calcular = New System.Windows.Forms.Button()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.salida, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cs
+        '
+        Me.cs.AutoSize = True
+        Me.cs.Location = New System.Drawing.Point(66, 93)
+        Me.cs.Name = "cs"
+        Me.cs.Size = New System.Drawing.Size(63, 13)
+        Me.cs.TabIndex = 1
+        Me.cs.Text = "Dame cifras"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(256, 93)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'salida
+        '
+        Me.salida.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.salida.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.salida.Location = New System.Drawing.Point(69, 137)
+        Me.salida.Name = "salida"
+        Me.salida.Size = New System.Drawing.Size(344, 469)
+        Me.salida.TabIndex = 4
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Valor i"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Serie Fibonacci"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Número Aureo"
+        Me.Column3.Name = "Column3"
+        '
+        'calcular
+        '
+        Me.calcular.Location = New System.Drawing.Point(541, 137)
+        Me.calcular.Name = "calcular"
+        Me.calcular.Size = New System.Drawing.Size(75, 23)
+        Me.calcular.TabIndex = 5
+        Me.calcular.Text = "Calcular"
+        Me.calcular.UseVisualStyleBackColor = True
+        '
+        'Salir
+        '
+        Me.Salir.Location = New System.Drawing.Point(541, 187)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(75, 23)
+        Me.Salir.TabIndex = 6
+        Me.Salir.Text = "Salir"
+        Me.Salir.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(256, 62)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(184, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Cuantos numeros quieres que calcule"
+        '
+        'Serie_fibonacci
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(641, 618)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.calcular)
+        Me.Controls.Add(Me.salida)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.cs)
+        Me.Name = "Serie_fibonacci"
+        Me.Text = "Serie de Fibonacci"
+        CType(Me.salida, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cs As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents salida As DataGridView
+    Friend WithEvents calcular As Button
+    Friend WithEvents Salir As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Seriefibonacci/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Serie_fibonacci
+    Dim maxValor As Integer
+    Dim n As Integer
+    Dim j As Integer
+    Dim redon As Integer
+    Dim ec As Single
+    Dim fibonacci As Single
+
+    Private Sub calcular_Click(sender As Object, e As EventArgs) Handles calcular.Click
+        j = 0
+        maxValor = TextBox3.Text
+        n = TextBox2.Text
+        Dim serieFibo(maxValor) As Double
+        Dim aureo(maxValor) As Single
+        redon = n + 2
+        Dim antNum, actNum As Single
+        Do While j < maxValor
+            If (j = 0) Then
+                antNum = 0
+                actNum = 1
+            End If
+            fibonacci = antNum + actNum
+            serieFibo(j) = fibonacci
+            aureo(j) = Math.Round(actNum / antNum, redon)
+            antNum = actNum
+            actNum = fibonacci
+            salida.Rows.Add(j, serieFibo(j), aureo(j))
+            j = j + 1
+        Loop
+    End Sub
+
+    Private Sub Salir_Click(sender As Object, e As EventArgs) Handles Salir.Click
+        End
+    End Sub
+End Class
